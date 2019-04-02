334608f33e18875abf4dc8317ac1e3d33017b4d2
Alan Parker Lue <aparkerlue@gmail.com>
2019-04-02 16:01:18 -0400

    Open man pages in current window

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,6 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-notify-method (quote pushy))
  '(custom-safe-themes
    (quote
     ("a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
